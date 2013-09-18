42f9dac0cd4a1084cae81d93f035df1efef7871e
David Lin <davll.xc@gmail.com>
2013-09-19 01:41:41 +0800

    improved testing

--- test/api/database_test.rb
+++ test/api/database_test.rb
@@ -24,24 +24,34 @@ class API_DatabaseTest < MiniTest::Test
         assert_kind_of Array, info['return_type']
         assert_kind_of Array, info['parameters']
         # check return type
-        info['return_type'].each{|t|
-          assert_kind_of String, t
-          assert_match /\A(\w+|\*)\z/, t
-        }
+        assert_return_type info['return_type']
         # check parameters
         info['parameters'].each{|p|
           assert_kind_of Array, p
           # check param type
-          p[0..-2].each{|t|
-            assert_kind_of String, t
-            assert_match /\A(\w+|\*)\z/, t
-          }
+          assert_parameter_type p[0..-2]
           # check param name
           assert_kind_of String, p.last
           assert_match /\A\w+\z/, p.last
         }
       end
     end
+  end
+  
+  def assert_return_type(type_ary)
+    assert_equal 1, type_ary.length
+    assert_kind_of String, type_ary[-1]
+    assert_includes %w(
+      void string pointer
+      GLenum GLboolean GLint GLuint GLsync
+    ), type_ary[-1]
+  end
+  
+  def assert_parameter_type(type_ary)
+    type_ary.each{|t|
+      assert_kind_of String, t
+      assert_match /\A(\w+|\*)\z/, t
+    }
   end
   
 end
