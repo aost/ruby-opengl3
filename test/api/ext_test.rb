72f08b6e8a89c39bd6e41203ddfbe54d88abb9fb
David Lin <davll.xc@gmail.com>
2013-09-28 01:19:48 +0800

    more extension

--- test/api/ext_test.rb
+++ test/api/ext_test.rb
@@ -13,4 +13,14 @@ class API_ExtTest < MiniTest::Test
     assert_respond_to mod, :glPrimitiveRestartIndexNV
   end
   
+  def test_ext2
+    mod = OpenGL::API::Builder.build(Context.new) do |builder|
+      OpenGL::API::Extension['GL_EXT_texture_compression_s3tc'].add_to builder
+    end
+    assert_operator mod, :const_defined?, :GL_COMPRESSED_RGB_S3TC_DXT1_EXT
+    assert_operator mod, :const_defined?, :GL_COMPRESSED_RGBA_S3TC_DXT1_EXT
+    assert_operator mod, :const_defined?, :GL_COMPRESSED_RGBA_S3TC_DXT3_EXT
+    assert_operator mod, :const_defined?, :GL_COMPRESSED_RGBA_S3TC_DXT5_EXT
+  end
+  
 end
